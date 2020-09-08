9df32839ad2e2867dbfee1e4f1078b3d9112ff73
ctrung2k <ctrung2k@gmail.com>
2020-09-08 14:11:10 +0700

    Data base

---
+++ DataBase_BanHang/qlcuahang.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Product(	--sản phẩm
+Id INT(11) auto INCREMENT PRIMARY KEY,
+TypeID (11),
+Name Varchar(200) not null,
+UnitPrice INT(11) not null,
+Description Varchar(200) not null,
+);
+
+CREATE TABLE ReciptDetail(	--chi tiết hóa đơn
+ReceiptTime datetime,
+ProductId INT(11),
+ReceiptId INT(11),
+Amount INT(6) not null,
+);
+
+create table Supplier(	--nhà cung cấp
+SupplierID int(11) auto increment primary key,
+ProductId INT(11),
+ProductName varchar(200) not null,
+ProductPrice INT(11),
+Amount INT(6) not null,
+);
+
+create table Storage(	--kho
+ProductId int(11),
+ProductName varchar(200),
+Amount int(11),
+)
+
+create table Revenue(	--doanh thu
+RevenueDay int(11),
+RevenueMonth int(11),
+)
+
+create table ProductType(	--loại sản phẩm
+TypeID int(11), auto increment primary key,
+TypeName varchar(200),
+)
+
+create table Cart(      --giỏ hàng
+ProductName varchar(200),
+Amount int(11),
+UnitPrice int(11),
+TotalPrice int(11), -- total price = amount * unit price
+)
+
+create table HistoryTransaction(	--lịch sử giao dịch
+
+)
