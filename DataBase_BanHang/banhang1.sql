b836f0ffca35b89f747c4fe31845d8e5b9dd2d50
danghao12 <vudanghao12@gmail.com>
2020-09-09 13:59:40 +0700

    moi hai

--- DataBase_BanHang/banhang1.sql
+++ DataBase_BanHang/banhang1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th9 09, 2020 lúc 04:53 AM
+-- Thời gian đã tạo: Th9 09, 2020 lúc 08:58 AM
 -- Phiên bản máy phục vụ: 10.4.14-MariaDB
 -- Phiên bản PHP: 7.4.9
 
@@ -24,37 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `cart`
---
-
-CREATE TABLE `cart` (
-  `ProductName` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Amount` int(11) NOT NULL,
-  `UnitPrice` double NOT NULL,
-  `TotalPrice` double NOT NULL,
-  `ID` int(11) NOT NULL,
-  `Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `customeraccount`
---
-
-CREATE TABLE `customeraccount` (
-  `Email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Pass` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Phone` int(11) NOT NULL,
-  `Gender` tinyint(1) NOT NULL,
-  `ID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `product`
 --
 
@@ -63,26 +32,26 @@ CREATE TABLE `product` (
   `Name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `UnitPrice` double NOT NULL,
   `TypeID` int(11) NOT NULL,
-  `SupplierID` int(11) NOT NULL,
   `Image` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Description` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+  `Description` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `Amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `product`
 --
 
-INSERT INTO `product` (`ID`, `Name`, `UnitPrice`, `TypeID`, `SupplierID`, `Image`, `Description`) VALUES
-(11, 'Coke (chai nhựa)', 7000, 1, 1, '', ''),
-(12, 'Diet coke (lon)', 7000, 1, 1, '', ''),
-(13, 'Fanta cam (chai nhựa)', 7000, 1, 1, '', ''),
-(14, 'Soda chanh (lon)', 9000, 1, 1, '', ''),
-(15, 'Nước Aquarius (chai nhựa)', 6000, 1, 1, '', ''),
-(16, 'Nước uống tăng lực Samurai (chai nhựa)', 10000, 1, 1, '', ''),
-(17, 'Nước trái cây Minute Maid (chai nhựa)', 8500, 1, 1, '', ''),
-(18, 'Nước khoáng Dasani (chai nhựa)', 5000, 1, 1, '', ''),
-(19, 'Sữa trái cây Nutriboost hương dâu (chai nhựa)', 10000, 1, 1, '', ''),
-(20, 'Sữa trái cây Nutriboost hương cam (chai nhựa)', 10000, 1, 1, '', '');
+INSERT INTO `product` (`ID`, `Name`, `UnitPrice`, `TypeID`, `Image`, `Description`, `Amount`) VALUES
+(11, 'Coke (chai nhựa)', 7000, 1, '', '', 0),
+(12, 'Diet coke (lon)', 7000, 1, '', '', 0),
+(13, 'Fanta cam (chai nhựa)', 7000, 1, '', '', 0),
+(14, 'Soda chanh (lon)', 9000, 1, '', '', 0),
+(15, 'Nước Aquarius (chai nhựa)', 6000, 1, '', '', 0),
+(16, 'Nước uống tăng lực Samurai (chai nhựa)', 10000, 1, '', '', 0),
+(17, 'Nước trái cây Minute Maid (chai nhựa)', 8500, 1, '', '', 0),
+(18, 'Nước khoáng Dasani (chai nhựa)', 5000, 1, '', '', 0),
+(19, 'Sữa trái cây Nutriboost hương dâu (chai nhựa)', 10000, 1, '', '', 0),
+(20, 'Sữa trái cây Nutriboost hương cam (chai nhựa)', 10000, 1, '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -121,47 +90,6 @@ CREATE TABLE `reciptdetail` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `revenue`
---
-
-CREATE TABLE `revenue` (
-  `RevenueDay` double NOT NULL,
-  `RevenueMonth` double NOT NULL,
-  `StatisticsDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `staffaccount`
---
-
-CREATE TABLE `staffaccount` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Phone` int(11) NOT NULL,
-  `Address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `PASS` int(11) NOT NULL,
-  `Gender` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `storage`
---
-
-CREATE TABLE `storage` (
-  `ProductId` int(11) NOT NULL,
-  `ProducName` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `Amount` int(11) NOT NULL,
-  `Ngaynhap` date NOT NULL,
-  `TypeId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `supplier`
 --
 
@@ -184,24 +112,11 @@ INSERT INTO `supplier` (`SupplierID`, `SupplierName`, `SupplierAddress`, `Suppli
 --
 
 --
--- Chỉ mục cho bảng `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `customeraccount`
---
-ALTER TABLE `customeraccount`
-  ADD PRIMARY KEY (`ID`);
-
---
 -- Chỉ mục cho bảng `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `pd_Typeid_pdt_ID` (`TypeID`),
-  ADD KEY `pd_supid_sup_supid` (`SupplierID`);
+  ADD KEY `pd_Typeid_pdt_ID` (`TypeID`);
 
 --
 -- Chỉ mục cho bảng `producttype`
@@ -216,19 +131,6 @@ ALTER TABLE `reciptdetail`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Chỉ mục cho bảng `staffaccount`
---
-ALTER TABLE `staffaccount`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `storage`
---
-ALTER TABLE `storage`
-  ADD KEY `id_sto_id_prod` (`ProductId`),
-  ADD KEY `TypeId` (`TypeId`);
-
---
 -- Chỉ mục cho bảng `supplier`
 --
 ALTER TABLE `supplier`
@@ -237,18 +139,6 @@ ALTER TABLE `supplier`
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
-
---
--- AUTO_INCREMENT cho bảng `cart`
---
-ALTER TABLE `cart`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `customeraccount`
---
-ALTER TABLE `customeraccount`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
@@ -269,12 +159,6 @@ ALTER TABLE `reciptdetail`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `staffaccount`
---
-ALTER TABLE `staffaccount`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT cho bảng `supplier`
 --
 ALTER TABLE `supplier`
@@ -288,15 +172,7 @@ ALTER TABLE `supplier`
 -- Các ràng buộc cho bảng `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `pd_Typeid_pdt_ID` FOREIGN KEY (`TypeID`) REFERENCES `producttype` (`ID`),
-  ADD CONSTRAINT `pd_supid_sup_supid` FOREIGN KEY (`SupplierID`) REFERENCES `supplier` (`SupplierID`);
-
---
--- Các ràng buộc cho bảng `storage`
---
-ALTER TABLE `storage`
-  ADD CONSTRAINT `id_sto_id_prod` FOREIGN KEY (`ProductId`) REFERENCES `product` (`ID`),
-  ADD CONSTRAINT `storage_ibfk_1` FOREIGN KEY (`TypeId`) REFERENCES `producttype` (`ID`);
+  ADD CONSTRAINT `pd_Typeid_pdt_ID` FOREIGN KEY (`TypeID`) REFERENCES `producttype` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
