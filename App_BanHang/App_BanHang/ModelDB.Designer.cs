d4f835d72f59db047e60147766410b405bd5eb3d
ctrung2k <ctrung2k@gmail.com>
2020-09-22 16:18:58 +0700

    DB

--- App_BanHang/App_BanHang/ModelDB.Designer.cs
+++ App_BanHang/App_BanHang/ModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\VU LEE\source\repos\letranvinhthang\App_BanHang\App_BanHang\ModelDB.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Trung\BanHang\App_BanHang\App_BanHang\ModelDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
